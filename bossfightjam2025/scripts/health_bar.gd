1805a6dca00c228fbee36802a3df834cf2bb994c
kokiheey <rivilttm@gmail.com>
2025-07-27 20:54:08 +0200

    Implement health bar following player

--- bossfightjam2025/scripts/health_bar.gd
+++ bossfightjam2025/scripts/health_bar.gd
@@ -1,10 +1,11 @@
 extends TextureProgressBar
 class_name HealthBar
 
-@export var player: PlayerCharacter
+@onready var player: PlayerCharacter = $"../.."
 
 func _ready():
 	value = player.health / player.MaxHealth * 100
 
 func _process(delta: float) -> void:
 	value = float(player.health) / player.MaxHealth * 100
+	global_position = player.global_position + Vector2(-55, -90)
