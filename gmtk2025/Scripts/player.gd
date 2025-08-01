0017bc16eb5d66ea1cc8136a66d3de1178633914
Tesla <tesla89karate@gmail.com>
2025-08-01 22:41:04 +0200

    Add tilemap

--- gmtk2025/Scripts/player.gd
+++ gmtk2025/Scripts/player.gd
@@ -8,15 +8,8 @@ func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
 		self.velocity = direction * SPEED
-		#za flipovanje karaktera
-		if direction == "ui_left":
-			pass
-		elif direction == "ui_left":
-			pass
-		elif direction == "ui_up":
-			pass
-		else:
-			pass
+		#Implementirati flipovanje karaktera
+ 
 		#----------
 	else:
 		self.velocity = Vector2.ZERO
