9d8237af270908119f6c5e76832e25240978934d
ItsClash <veljko.lukajic@gmail.com>
2025-08-02 18:59:47 +0200

    fix button

--- gmtk2025/pressure_plate.gd
+++ gmtk2025/pressure_plate.gd
@@ -2,13 +2,15 @@ extends StaticBody2D
 
 @export var toggles: Array[Toggle]
 
+@onready var sprite : Sprite2D = $Sprite2D
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	self.position.y += 1;
+	sprite.scale *= 0.94
 	for tg in toggles:
 		tg.toggle()
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	self.position.y -= 1;
+	sprite.scale = Vector2.ONE
 	for tg in toggles:
 		tg.toggle()
