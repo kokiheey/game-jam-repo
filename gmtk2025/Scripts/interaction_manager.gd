fc43367d728801729a49ea75fcc91df2493f71d2
koki <152427234+kokiheey@users.noreply.github.com>
2025-08-02 19:00:28 +0200

    Fix interaction manager

--- gmtk2025/Scripts/interaction_manager.gd
+++ gmtk2025/Scripts/interaction_manager.gd
@@ -1,8 +1,9 @@
-class_name InteractionManager
 extends Node2D
+class_name InteractionManager
 
-# @onready var player: CharacterBody2D = get_tree().get_node("Player")
-# @onready var label: Label = $InteractLabel
+
+@onready var player: CharacterBody2D = get_tree().get_node("Player")
+@onready var label: Label = $InteractLabel
 var active_areas: Array[InteractableArea]
 var can_interact: bool = true
 
@@ -11,19 +12,19 @@ const base_text: String = "[E] to "
 func register_area(area: InteractableArea):
 	active_areas.push_back(area)
 
-func unregister_area(area: InteractableArea:)
+func unregister_area(area: InteractableArea):
 	var index = active_areas.find(area)
 	if index != -1:
-		area[index] = area.back()
-		area.pop_back()
+		active_areas[index] = area.back()
+		active_areas.pop_back()
 
 func _process(delta: float) -> void:
 	if active_areas.size() > 0 and can_interact:
 		active_areas.sort_custom(func(a:Area2D, b:Area2D): return a.distance_squared_to(player.global_position) < \
 		b.distance_squared_to(player.global_position))
 
-		var currentArea: InteractableArea = active_areas
-		label.text = base_text + currentArea.active_text + action_name
+		var currentArea: InteractableArea = active_areas[0]
+		label.text = base_text + currentArea.action_name
 		label.global_position = currentArea.global_position
 		label.global_position.y -= 20
 		label.global_position.x = label.size.x / 2
