584eee839df873f3ade40f5e2ad0d46218ba68ad
koki <152427234+kokiheey@users.noreply.github.com>
2025-08-02 14:58:14 +0200

    Add togglable base class and implement toggle array functionality in pressure_plate

---
+++ gmtk2025/Scripts/toggle.gd
@@ -0,0 +1,7 @@
+extends Node2D
+class_name Toggle
+
+var toggled: bool
+
+func toggle():
+    pass
--- gmtk2025/pressure_plate.gd
+++ gmtk2025/pressure_plate.gd
@@ -1,9 +1,15 @@
 extends StaticBody2D
 
 
+@export var toggles: Array[Toggle]
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	self.position.y += 1;
+	for tg in toggles:
+		tg.toggle()
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	self.position.y -= 1;
+	for tg in toggles:
+		tg.toggle()
