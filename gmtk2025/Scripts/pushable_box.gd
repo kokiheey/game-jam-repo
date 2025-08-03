73eac2dd4fcfe28145609f0731720f7ef6404ce9
ItsClash <veljko.lukajic@gmail.com>
2025-08-03 16:25:46 +0200

    can move box with interaction key

--- gmtk2025/Scripts/pushable_box.gd
+++ gmtk2025/Scripts/pushable_box.gd
@@ -11,8 +11,9 @@ func _ready():
 	upRight.body_exited.connect(_on_up_right_body_exited)
 	downLeft.body_exited.connect(_on_down_left_body_exited)
 	downRight.body_exited.connect(_on_down_right_body_exited)
-
+	
 var can_move := true
+var offset := Vector2.ZERO
 var disabled_directions := {
 	"up_left": false,
 	"up_right": false,
@@ -25,43 +26,51 @@ func move(offset: Vector2):
 	position += offset
 	can_move = true
 
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		print(offset)
+		move(offset)
+
 func _on_up_left_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
 		disabled_directions["down_right"] = true
 	elif body is CharacterBody2D and !disabled_directions["up_left"] and can_move:
-		move(Vector2(-16, -8))
+		offset = Vector2(-16, -8)
 
 func _on_up_right_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
 		disabled_directions["down_left"] = true
 	elif body is CharacterBody2D and !disabled_directions["up_right"] and can_move:
-		move(Vector2(16, -8))
+		offset = Vector2(16, -8)
 
 func _on_down_left_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
 		disabled_directions["up_right"] = true
 	elif body is CharacterBody2D and !disabled_directions["down_left"] and can_move:
-		move(Vector2(-16, 8))
+		offset = Vector2(-16, 8)
 
 func _on_down_right_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
 		disabled_directions["up_left"] = true
 	elif body is CharacterBody2D and !disabled_directions["down_right"] and can_move:
-		move(Vector2(16, 8))
-
+		offset = Vector2(16, 8)
 
 func _on_up_left_body_exited(body: Node2D) -> void:
 	if body is TileMapLayer:
 		disabled_directions["down_right"] = false
+	offset = Vector2.ZERO
 
 func _on_up_right_body_exited(body: Node2D) -> void:
 	if body is TileMapLayer:
 		disabled_directions["down_left"] = false
+	offset = Vector2.ZERO
 
 func _on_down_left_body_exited(body: Node2D) -> void:
 	if body is TileMapLayer:
 		disabled_directions["up_right"] = false
+	offset = Vector2.ZERO
 
 func _on_down_right_body_exited(body: Node2D) -> void:
 	if body is TileMapLayer:
 		disabled_directions["up_left"] = false
+	offset = Vector2.ZERO
