5de206a2f764448cf4fa06af6c79b9a2181ac5bb
Tesla <tesla89karate@gmail.com>
2025-08-03 02:21:23 +0200

    Improve replay
    Co-Authored-By: koki <152427234+kokiheey@users.noreply.github.com>

--- gmtk2025/Scripts/replay_manager.gd
+++ gmtk2025/Scripts/replay_manager.gd
@@ -6,7 +6,7 @@ extends Node
 @export var player: CharacterBody2D
 @onready var player_sprite: AnimatedSprite2D = player.get_node("AnimatedSprite2D")
 @export var spawn_position: Node2D
-@export var MAX_GHOSTS: int = 2
+@export var MAX_GHOSTS: int = 3
 var toggle_data_on: Dictionary = {}
 var toggle_data_off: Dictionary = {}
 var frames: int
@@ -20,7 +20,12 @@ var ghost_bodies: Array[AnimatedSprite2D]
 func _ready():
 	ghosts = 0
 	frames = 0
-	
+	for index in MAX_GHOSTS:
+		var new_ghost = ghost_prefab.instantiate()
+		ghost_bodies.append(new_ghost)
+		new_ghost.global_position = spawn_position.position
+		ghost_bodies[index].hide()
+		objects.add_child(new_ghost)
 	for ob in objects.get_children():
 		if ob is Toggle:
 			var tg = ob as Toggle
@@ -34,11 +39,11 @@ func _physics_process(delta: float):
 		for tg in replay_toggles:
 			tg.on_toggles = 0
 	frames += 1
-	if toggle_data_on.has(frames):
-		toggle_data_on[frames].toggle_on(true)
-	if toggle_data_off.has(frames):
-		toggle_data_off[frames].toggle_off(true)
 	
+	if not toggle_data_on.has(ghosts):
+		toggle_data_on[ghosts] = {}
+	if not toggle_data_off.has(ghosts):
+		toggle_data_off[ghosts] = {}
 	if not ghosts_position.has(ghosts): 
 		ghosts_position[ghosts] = {}
 	if not ghosts_animations.has(ghosts):
@@ -46,7 +51,18 @@ func _physics_process(delta: float):
 		
 	ghosts_animations[ghosts][frames] = player_sprite.animation
 	ghosts_position[ghosts][frames] = player.global_position
-	for i in ghost_bodies.size():
+	
+	
+	for i in MAX_GHOSTS:
+		if i == ghosts: 
+			ghost_bodies[i].hide()
+			continue
+		if not ghosts_position.has(i):
+			ghost_bodies[i].hide()
+			continue
+		#(ghost - i + MAX_GHOSTS) % MAX_GHOSTS
+		ghost_bodies[i].modulate.a = 1.0 - (((ghosts - i + MAX_GHOSTS) % MAX_GHOSTS) / float(MAX_GHOSTS))
+		ghost_bodies[i].show()
 		if ghosts_position[i].has(frames): 
 			ghost_bodies[i].global_position = ghosts_position[i][frames]
 		if ghosts_animations[i].has(frames) and frames >= 2:
@@ -56,19 +72,27 @@ func _physics_process(delta: float):
 			if ghosts_position[i][frames] == ghosts_position[i][frames-1]:
 				ghost_bodies[i].stop()
 				ghost_bodies[i].frame = 1
+		if toggle_data_off[i].has(frames):
+			toggle_data_off[i][frames].toggle_off(true)
+		if toggle_data_on[i].has(frames):
+			toggle_data_on[i][frames].toggle_on(true)
 func _on_toggled_on(sender: Toggle):
-	toggle_data_on[frames] = sender
+	toggle_data_on[ghosts][frames] = sender
 
 func _on_toggled_off(sender: Toggle):
-	toggle_data_off[frames] = sender
+	toggle_data_off[ghosts][frames] = sender
 
 func replay():
 	frames = 0
 	ghosts += 1
-	var new_ghost = ghost_prefab.instantiate()
-	ghost_bodies.append(new_ghost)
-	new_ghost.global_position = spawn_position.position
-	objects.add_child(new_ghost)
+	ghosts %= MAX_GHOSTS
+	
+	toggle_data_off[ghosts] = {}
+	toggle_data_on[ghosts] = {}
+	ghosts_position[ghosts] = {}
+	ghosts_animations[ghosts] = {}
+	
+	
 	
 	player.global_position = spawn_position.global_position
 	for tg in replay_toggles:
