63514bed00d6bd1fc7eaa84254a9f141291fa572
kokiheey <rivilttm@gmail.com>
2025-08-03 16:21:53 +0200

    Fix replay manager looping overwriting previous data

--- gmtk2025/Scripts/door.gd
+++ gmtk2025/Scripts/door.gd
@@ -5,8 +5,8 @@ extends Toggle
 func toggle_on(echo: bool):
 	on_toggles += 1
 	print("tg ", on_toggles)
+	if not echo: on_toggled_on.emit(self)
 	if on_toggles > 0:
-		if not echo: on_toggled_on.emit(self)
 		body.collision_layer = 0
 		sprite.hide()
 
@@ -14,8 +14,8 @@ func toggle_off(echo: bool):
 	on_toggles -= 1
 	on_toggles = max(0, on_toggles)
 	print("tg ", on_toggles)
+	if not echo: on_toggled_off.emit(self)
 	if on_toggles <= 0:
-		if not echo: on_toggled_off.emit(self)
 		body.collision_layer = 1
 		sprite.show()
 	
