dd494792c8ba8d5832efceda427f1b754158cf2f
ItsClash <veljko.lukajic@gmail.com>
2025-08-03 02:21:07 +0200

    Fix box

--- gmtk2025/Scripts/pushable_box.gd
+++ gmtk2025/Scripts/pushable_box.gd
@@ -6,44 +6,40 @@ class_name Box
 @onready var downLeft : Area2D = $down_left
 @onready var downRight : Area2D = $down_right
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var can_move := true
+var disabled_directions := {
+	"up_left": false,
+	"up_right": false,
+	"down_left": false,
+	"down_right": false
+}
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+func move(offset: Vector2):
+	can_move = false
+	position += offset
+	await get_tree().create_timer(0.1).timeout
+	can_move = true
 
 func _on_up_left_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
-		downRight.hide()
-	elif body is CharacterBody2D:
-		position.x -= 16
-		position.y -= 8
-		
-
+		disabled_directions["down_right"] = true
+	elif body is CharacterBody2D and !disabled_directions["up_left"] and can_move:
+		move(Vector2(-16, -8))
 
 func _on_up_right_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
-		downLeft.hide()
-	elif body is CharacterBody2D:
-		position.x += 16
-		position.y -= 8
-
+		disabled_directions["down_left"] = true
+	elif body is CharacterBody2D and !disabled_directions["up_right"] and can_move:
+		move(Vector2(16, -8))
 
 func _on_down_left_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
-		upRight.hide()
-	elif body is CharacterBody2D:
-		position.x -= 16
-		position.y += 8
-
+		disabled_directions["up_right"] = true
+	elif body is CharacterBody2D and !disabled_directions["down_left"] and can_move:
+		move(Vector2(-16, 8))
 
 func _on_down_right_body_entered(body: Node2D) -> void:
 	if body is TileMapLayer:
-		upLeft.hide()
-	elif body is CharacterBody2D:
-		position.x += 16
-		position.y += 8
+		disabled_directions["up_left"] = true
+	elif body is CharacterBody2D and !disabled_directions["down_right"] and can_move:
+		move(Vector2(16, 8))
