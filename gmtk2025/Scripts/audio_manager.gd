7725915a6ce464319a1c7d1212685289445f3b7b
svuskovic22 <tesla89karate@gmail.com>
2025-08-03 16:29:50 +0200

    Fix music muting

--- gmtk2025/Scripts/audio_manager.gd
+++ gmtk2025/Scripts/audio_manager.gd
@@ -2,13 +2,20 @@ extends Node
 
 @onready var player: AudioStreamPlayer = $AudioStreamPlayer
 
+var mute : bool = false
+
 func mute_music(toggled: bool) -> void:
 	if toggled:
-		player.playing = false
+		mute = true
 	else:
-		player.playing = true
+		mute = false
+	
+	if mute:
+		player.stop()
+	else:
+		player.play()
 
 func play_music(stream: AudioStream) -> void:
 	if player.stream != stream:
 		player.stream = stream
-		player.play()
+		if !mute: player.play()
