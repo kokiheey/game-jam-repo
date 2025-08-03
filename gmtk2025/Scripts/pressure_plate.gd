9641fee7e084f5500e2fdc1fa0e08229e33e88d5
svuskovic22 <tesla89karate@gmail.com>
2025-08-03 17:15:44 +0200

    Fix pressure plate

--- gmtk2025/Scripts/pressure_plate.gd
+++ gmtk2025/Scripts/pressure_plate.gd
@@ -2,24 +2,24 @@ extends Area2D
 
 @export var toggles: Array[Toggle]
 
-@onready var sprite : Sprite2D = $Sprite2D
+@onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _on_body_entered(body: Node2D) -> void:
-	sprite.scale *= 0.88
+	sprite.frame = 1
 	if body is CharacterBody2D: for tg in toggles:
 		tg.toggle_on(false)
 
 func _on_body_exited(body: Node2D) -> void:
-	sprite.scale = Vector2.ONE
+	sprite.frame = 0
 	if body is CharacterBody2D : for tg in toggles:
 		tg.toggle_off(false)
 
 func _on_area_entered(area: Area2D) -> void:
-	sprite.scale *= 0.88
+	sprite.frame = 1
 	for tg in toggles:
 		tg.toggle_on(false)
 
 func _on_area_exited(area: Area2D) -> void:
-	sprite.scale = Vector2.ONE
+	sprite.frame = 0
 	for tg in toggles:
 		tg.toggle_off(false)
