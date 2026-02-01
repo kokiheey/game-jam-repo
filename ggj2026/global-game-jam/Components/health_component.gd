fb05a17db7237ab89399c687777be84999449cf1
kokiheey <rivilttm@gmail.com>
2026-02-01 14:07:14 +0100

    ne znam vise

--- ggj2026/global-game-jam/Components/health_component.gd
+++ ggj2026/global-game-jam/Components/health_component.gd
@@ -1,10 +1,13 @@
 extends Node
-
+class_name health_component
 @export var MAX_HEALTH: float = 10.0
 var health: float
 
+signal dead
 func _ready():
 	health = MAX_HEALTH
-	
-func damage(attack):
-	
+
+func damage(attack: Attack):
+	health -= attack.attack_damage
+	if health <= 0:
+		dead.emit()
--- ggj2026/global-game-jam/Enemy/enemy.gd
+++ ggj2026/global-game-jam/Enemy/enemy.gd
@@ -1,12 +1,14 @@
 extends RigidBody2D
 @export var player: CharacterBody2D
 @export var movespeed : float = 50
-# Called when the node enters the scene tree for the first time.
+@onready var health = $health_component
+
+func onDeath():
+	print("i am dead")
+
 func _ready() -> void:
-	pass # Replace with function body.
+	health.dead.connect(onDeath)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var distance: Vector2 = global_position - player.global_position
 	var dir: Vector2 = distance.normalized()
