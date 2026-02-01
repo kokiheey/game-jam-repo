afd357fbb3676d59c355591573bc067d2237e1ca
kokiheey <rivilttm@gmail.com>
2026-02-01 15:19:11 +0100

    Make enemies reveal map on death

--- ggj2026/global-game-jam/Player/Components/Interaction/interaction.gd
+++ ggj2026/global-game-jam/Player/Components/Interaction/interaction.gd
@@ -29,7 +29,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		current_interactions.push_back(area)
 
 
-func _on_area_2d_area_exited(area: Area2D) -> void:
+func _on_area_2d_area_edwxited(area: Area2D) -> void:
 	if area is Interactable:
 		area.interaction_label.hide()
 		current_interactions.erase(area)
