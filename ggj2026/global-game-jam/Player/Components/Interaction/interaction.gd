3fe2254b38c7fe4ddfff55c7ebcdbbd7cb291bcf
kokiheey <rivilttm@gmail.com>
2026-02-01 14:50:05 +0100

    Add damage

--- ggj2026/global-game-jam/Player/Components/Interaction/interaction.gd
+++ ggj2026/global-game-jam/Player/Components/Interaction/interaction.gd
@@ -11,7 +11,7 @@ func process_interaction() -> void:
 func _process(_delta: float) -> void:
 	if not current_interactions: return
 	for a in current_interactions:
-		a.label_text
+		pass
 	if can_interact:
 		current_interactions.sort_custom(_sort_by_nearest)
 		if current_interactions[0].is_interactable:
