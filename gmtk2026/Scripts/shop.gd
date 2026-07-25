4ce391a114dab6cb2d8439017cdb95470cdcf082
kokiheey <rivilttm@gmail.com>
2026-07-26 00:49:05 +0200

    Fix shop.gd

--- gmtk2026/Scripts/shop.gd
+++ gmtk2026/Scripts/shop.gd
@@ -11,19 +11,19 @@ var money : int
 @export var pickupSpeedCost  : int = 4
 var upgrades : Array = []
 
-func _ready():
-	upgrades = [
-		{ "cost": speedUpgradeCost    , "signal": boughtSpeed},
-		{ "cost": waypointDistanceCost, "signal": boughtWaypointDistance},
-		{ "cost": pickupSpeedCost     , "signal": boughtPickupSpeed },
-	]
-	$SpeedButton.pressed.connect(try_purchase(0))
-	$WaypointDistButton.pressed.connect(try_purchase(1))
-	$CollectionSpeed.pressed.connect(try_purchase(2))
-
 func try_purchase(index : int):
 	var upgrade = upgrades[index]
 	if money >= upgrade["cost"]:
 		money -= upgrade["cost"]
 		upgrade["signal"].emit()
 		moneyChanged.emit(money)
+
+func _ready():
+	upgrades = [
+		{ "cost": speedUpgradeCost    , "signal": boughtSpeed},
+		{ "cost": waypointDistanceCost, "signal": boughtWaypointDistance},
+		{ "cost": pickupSpeedCost     , "signal": boughtPickupSpeed },
+	]
+	$SpeedButton.pressed.connect(try_purchase.bind(0))
+	$WaypointDistButton.pressed.connect(try_purchase.bind(1))
+	$CollectionSpeed.pressed.connect(try_purchase.bind(2))
